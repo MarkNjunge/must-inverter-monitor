1ed134f423694fba60d91752e4494d1c641ce6f7
Shus Sergii <45815042+shussergii@users.noreply.github.com>
2024-12-21 18:50:46 +0200

    feat: Add More data from modbus triggers
    added triggers for PV Charger 
    added triggers for Extermal BMS - works for Li-Battery if inverter connected  via CAN cable to battery

--- main.py
+++ main.py
@@ -73,9 +73,157 @@ register_map = {
     25233: ["acRadiatorTemperature", "AC radiator temperature", 1, "C"],
     25234: ["transformerTemperature", "Transformer temperature", 1, "C"],
     25235: ["dcRadiatorTemperature", "DC radiator temperature", 1, "C"],
+
+    25237: ["InverterRelayState", "InverterRelayState", 1, "on/off"],
+    25238: ["GridRelayState", "GridRelayState", 1, "on/off"],
+    25239: ["LoadRelayState", "LoadRelayState", 1, "on/off"],
+    25240: ["N_LineRelayState", "N_LineRelayState", 1, "on/off"],
+    25241: ["DCRelayState", "DCRelayState", 1, "on/off"],
+    25242: ["EarthRelayState", "EarthRelayState", 1, "on/off"],
+
+    25245: ["AccumulatedChargerPowerM", "AccumulatedChargerPowerM", 1, "MWh"],
+    25246: ["AccumulatedChargerPower", "AccumulatedChargerPower", 1, "kWh"],
+
+    25247: ["AccumulatedDischargerPowerM", "AccumulatedDischargerPowerM", 1, "MWh"],
+    25248: ["AccumulatedDischargerPower", "AccumulatedDischargerPower", 1, "kWh"],
+
+    25249: ["AccumulatedBuyPowerM", "AccumulatedBuyPowerM", 1, "MWh"],
+    25250: ["AccumulatedBuyPower", "AccumulatedBuyPower", 1, "kWh"],
+
+    25251: ["AccumulatedSellPowerM", "AccumulatedSellPowerM", 1, "MWh"],
+    25252: ["AccumulatedSellPower", "AccumulatedSellPower", 1, "kWh"],
+
+    25253: ["AccumulatedLoadPowerM", "AccumulatedLoadPowerM", 1, "MWh"],
+    25254: ["AccumulatedLoadPower", "AccumulatedLoadPower", 1, "kWh"],
+
+    25255: ["AccumulatedSelfUsePowerM", "AccumulatedSelfUsePowerM", 1, "MWh"],
+    25256: ["AccumulatedSelfUsePower", "AccumulatedSelfUsePower", 1, "kWh"],
+
+    25257: ["AccumulatedPvSellPowerM", "AccumulatedPvSellPowerM", 1, "MWh"],
+    25258: ["AccumulatedPvSellPower", "AccumulatedPvSellPower", 1, "kWh"],
+
+    25259: ["AccumulatedGridChargerPowerM", "AccumulatedGridChargerPowerM", 1, "MWh"],
+    25260: ["AccumulatedGridChargerPower", "AccumulatedGridChargerPower", 1, "kWh"],
+
+    25261: ["InverterErrorMessage", "InverterErrorMessage", 1, ""],
+    25265: ["InverterWarningMessage", "InverterWarningMessage", 1, ""],
+
+
+
     25273: ["batteryPower", "Battery power", 1, "W"],
     25274: ["batteryCurrent", "Battery current", 1, "A"],
+
+    25279: ["ArrowFlag","Arrow Flag",1,""],
+    20109: [
+        "EnergyUseMode",
+        "Energy use mode",
+        1,
+        "map",
+        {
+            0: "-",
+            1: "SBU",
+            2: "SUB",
+            3: "UTI",
+            4: "SOL",
+        },
+    ],
+
+    20111: [
+        "Grid_protect_standard",
+        "Grid protect standard",
+        1,
+        "map",
+        {
+            0: "VDE4105",
+            1: "UPS",
+            2: "HOME",
+            3: "GEN",
+        },
+    ],
+
+    20112: [
+        "SolarUseAim",
+        "SolarUse Aim",
+        1,
+        "map",
+        {
+            0: "LBU",
+            1: "BLU",
+        },
+    ],
+    20113: ["Inv_max_discharger_cur", "Inverter max discharger current", 0.1, "A"],
+    20118: ["BatStopDischargingV", "Battery stop discharging voltage", 0.1, "V"],
+    20119: ["BatStopChargingV", "Battery stop charging voltage", 0.1, "V"],
+    20125: ["GridMaxChargerCurSet", "Grid max charger current set", 0.1, "A"],
+    20127: ["BatLowVoltage", "Battery low voltage", 0.1, "V"],
+    20128: ["BatHighVoltage", "Battery high voltage", 0.1, "V"],
+
+
+    15201: [
+        "ChargerWorkstate",
+        "Charger Workstate",
+        1,
+        "map",
+        {
+            0: "Initialization",
+            1: "Selftest",
+            2: "Work",
+            3: "Stop",
+        },
+    ],
+
+    15202: [
+        "MpptState",
+        "Mppt State",
+        1,
+        "map",
+        {
+            0: "Stop",
+            1: "MPPT",
+            2: "Current limiting",
+        },
+    ],
+
+    15203: [
+        "ChargingState",
+        "Charging State",
+        1,
+        "map",
+        {
+            0: "Stop",
+            1: "Absorb charge",
+            2: "Float charge",
+            3: "Equalization charge",
+        },
+    ],
+
+  15205: ["PvVoltage", "Pv. Voltage", 0.1, "V"],
+  15206: ["chBatteryVoltage", "Ch. Battery Voltage", 0.1, "V"],
+  15207: ["chChargerCurrent", "Ch. Charger Current", 0.1, "A"],
+  15208: ["ChargerPower", "Ch. Charger Power", 1, "W"],
+  15209: ["RadiatorTemperature", "Ch. Radiator Temperature", 1, "C"],
+  15210: ["ExternalTemperature", "Ch. External Temperature", 1, "C"],
+  15211: ["BatteryRelay", "Battery Relay", 1, ""],
+  15212: ["PvRelay", "Pv. Relay", 1, ""],
+  15213: ["ChargerErrorMessage", "Charger Error Message", 1, ""],
+  15214: ["ChargerWarningMessage", "Charger Warning Message", 1, ""],
+  15215: ["BattVolGrade", "BattVolGrade", 1, "V"],
+  15216: ["RatedCurrent", "Rated Current", 0.1, "A"],
+
+  15217: ["AccumulatedPowerM", "Accumulated PowerM", 1 , "MWh"],
+  15218: ["AccumulatedPower", "Accumulated Power", 1 , "kWh"],
+  15219: ["AccumulatedTimeDay", "Accumulated Time day", 1 , "d"],
+
+# Get data from External BMS
+  109: ["BMS_Battery_Voltage", "BMS Battery Voltage", 0.1 , "V"],
+  110: ["BMS_Battery_Current", "BMS Battery Current", 0.1 , "A"],
+  111: ["BMS_Battery_Temperature", "BMS Battery Temperature", 1 , "C"],
+  112: ["BMS_Battery_Errors", "BMS Battery Error", 1 , ""],
+  113: ["BMS_Battery_SOC", "BMS Battery SOC", 1 , "%"],
+  114: ["BMS_Battery_SOH", "BMS Battery SOH", 1 , "%"]
+
 }
+
 
 
 def read_register_values(i, startreg, count):
@@ -93,6 +241,12 @@ def read_register_values(i, startreg, count):
             else:
                 r_value = str(round(r * r_unit, 2))
 
+            # convert from offset val fix for Inverter power owerload
+            if register_id == 25213 or register_id == 25273 or register_id == 25274  or register_id == 25214 or register_id == 110 :
+                if float(r_value) > 32000 :
+                   r_value =- abs(float(r_value)- 65536)
+
+
             stats_line += r_key + "=" + str(r_value) + ","
 
         register_id += 1
@@ -100,7 +254,7 @@ def read_register_values(i, startreg, count):
     # Remove comma at the end
     stats_line = stats_line[:-1]
 
-    send_data(stats_line)
+    return stats_line
 
 
 def send_data(stats):
@@ -127,7 +281,12 @@ while infinite:
     i = minimalmodbus.Instrument(SERPORT, 4)
     i.serial.timeout = SERTIMEOUT
     i.serial.baudrate = SERBAUD
-    read_register_values(i, 25201, 75)
+    stats_line_all = read_register_values(i, 15201, 19)
+    stats_line_all += "," + read_register_values(i, 25201, 80)
+    stats_line_all += "," + read_register_values(i, 20109, 19)
+    stats_line_all += "," + read_register_values(i, 109, 6)
+    send_data(stats_line_all)
+
 
     # infinite = False
 
